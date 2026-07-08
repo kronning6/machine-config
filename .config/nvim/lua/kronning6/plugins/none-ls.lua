f20df68e1906c76f9f6842c9c2122ac9e2f0b265
Kyle Ronning <kyleronning+kronning6@gmail.com>
2026-07-08 12:20:50 -0500

    fix: update eslint and prettier to use non daemon processes and make work in monorepo

--- .config/nvim/lua/kronning6/plugins/none-ls.lua
+++ .config/nvim/lua/kronning6/plugins/none-ls.lua
@@ -9,18 +9,57 @@ return {
     local nls = require 'null-ls'
     local nls_utils = require 'null-ls.utils'
 
+    local eslint_config_files = {
+      'eslint.config.js',
+      'eslint.config.mjs',
+      'eslint.config.cjs',
+      '.eslintrc.json',
+      '.eslintrc.js',
+      '.eslintrc.cjs',
+    }
+
+    local function find_upward(start_dir, file_name)
+      local matches = vim.fs.find(file_name, { path = start_dir, upward = true })
+      return matches[1]
+    end
+
+    local function has_eslint_config(utils)
+      return utils.root_has_file(eslint_config_files)
+    end
+
+    local function eslint_cwd(params)
+      local config_file = find_upward(params.bufname, eslint_config_files)
+      if config_file ~= nil then
+        return vim.fs.dirname(config_file)
+      end
+
+      return params.root
+    end
+
+    local eslint_args = {
+      'exec',
+      'eslint',
+      '-f',
+      'json',
+      '--stdin',
+      '--stdin-filename',
+      '$FILENAME',
+    }
+
     nls.setup {
       root_dir = nls_utils.root_pattern('.null-ls-root', 'package.json', 'tsconfig.json'),
       sources = {
-        require('none-ls.diagnostics.eslint_d').with {
-          condition = function(utils)
-            return utils.root_has_file { '.eslintrc.json', '.eslintrc.js', '.eslintrc.cjs' }
-          end,
+        require('none-ls.diagnostics.eslint').with {
+          command = 'pnpm',
+          args = eslint_args,
+          cwd = eslint_cwd,
+          condition = has_eslint_config,
         },
-        require('none-ls.code_actions.eslint_d').with {
-          condition = function(utils)
-            return utils.root_has_file { '.eslintrc.json', '.eslintrc.js', '.eslintrc.cjs' }
-          end,
+        require('none-ls.code_actions.eslint').with {
+          command = 'pnpm',
+          args = eslint_args,
+          cwd = eslint_cwd,
+          condition = has_eslint_config,
         },
       },
     }
